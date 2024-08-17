041f8fd64955aee96806dcb3fc8a30dd854f221b
SAKUMAISAO130 <colorfullweb@gmail.com>
2024-08-18 05:42:17 +0900

    大枠の設定を作成

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,23 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+
+  // ルートパスの設定
+  trailingSlash: true,
+
+  // 画像の最適化
+  images: { unoptimized: true },
+
+  /**
+   * 静的サイトジェネレーションの設定
+   */ 
+  // エクスポートを有効にする
+  output: 'export',
+  // 出力先ディレクトリ
+  distDir: 'out',
+  // cssや画像などの静的ファイルのパスのプレフィックスなど
+  basePath: '/out',
+  assetsPrefix: './',
 };
 
 export default nextConfig;
